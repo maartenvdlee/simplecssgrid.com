2d0a724b354b8a3caacdb3d3c1889b7306750244
Maarten van der Lee <maarten.vd.lee@gmail.com>
2012-09-25 11:17:42 +0200

    Added existing sourcecode

---
+++ Web/Controllers/DocsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class DocsController : Controller
+    {
+        //
+        // GET: /Docs/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Demo(int? id)
+        {
+            if (id.HasValue && (id == 720 || id == 960 || id == 1080 || id == 1200))
+                ViewBag.GridSize = id.Value;
+
+            return View();
+        }
+
+    }
+}
